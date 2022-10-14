f2da880ed5dcf43b587864d1cfac4e300e9737f3
nurayklc <nuraykilic0101@gmail.com>
2022-10-14 13:52:04 +0300

    Add Service Layer

---
+++ SupportForum.Service/PostService.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupportForum.Data;
+using SupportForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportForum.Service
+{
+    public class PostService : IPost
+    {
+        private readonly ApplicationDbContext _context;
+        public PostService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Post post)
+        {
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditPostContent(int id, string newContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            return _context.Posts.
+                Include(post => post.User)
+                .Include(post => post.Replies).ThenInclude(reply => reply.User)
+                .Include(post => post.Forum);
+        }
+
+        public Post GetById(int id)
+        {
+            return _context.Posts.Where(post => post.Id == id)
+                .Include(post => post.User)
+                .Include(post => post.Replies)
+                    .ThenInclude(reply => reply.User)
+                .Include(post => post.Forum)
+                .First();
+        }
+
+        public IEnumerable<Post> GetFilteredPosts(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetLatestPosts(int n)
+        {
+            return GetAll().OrderByDescending(post => post.Created).Take(n);
+        }
+
+        public IEnumerable<Post> GetPostsByForum(int id)
+        {
+            return _context.Forums.Where(forum => forum.Id == id).First().Posts;
+        }
+    }
+}
